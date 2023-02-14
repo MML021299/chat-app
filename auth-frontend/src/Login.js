082c8f748534145e999fa2f2aabdabeb583af17e
mlumanta <lumanta.mikhail21299@gmail.com>
2023-02-14 20:45:09 +0800

    Routing changes

--- auth-frontend/src/Login.js
+++ auth-frontend/src/Login.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { Form, Button } from "react-bootstrap";
 import axios from "axios";
 import Cookies from "universal-cookie";
+import { Navigate } from "react-router-dom";
 
 export default function Login() {
     const [email, setEmail] = useState("");
@@ -9,6 +10,7 @@ export default function Login() {
     const [login, setLogin] = useState(false);
 
     const cookies = new Cookies();
+    const token = cookies.get("TOKEN");
 
     const configuration = {
         method: "post",
@@ -37,48 +39,55 @@ export default function Login() {
         });
     }
 
+    if(token) {
+        return <Navigate to='/auth' replace />
+    }
+
     return (
-        <>
-            <h2>Login</h2>
-            <Form onSubmit={(e)=>handleSubmit(e)}>
-                {/* email */}
-                <Form.Group controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control
-                    type="email"
-                    name="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    placeholder="Enter email"
-                />
-                </Form.Group>
+        <div className="d-flex flex-grow-1 justify-content-center align-items-center">
+            <div>
+                <h2>Login</h2>
+                <Form onSubmit={(e)=>handleSubmit(e)}>
+                    {/* email */}
+                    <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control
+                        type="email"
+                        name="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        placeholder="Enter email"
+                    />
+                    </Form.Group>
 
-                {/* password */}
-                <Form.Group controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                    type="password"
-                    name="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    placeholder="Password"
-                />
-                </Form.Group>
+                    {/* password */}
+                    <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                        type="password"
+                        name="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        placeholder="Password"
+                    />
+                    </Form.Group>
 
-                {/* submit button */}
-                <Button
-                    variant="primary"
-                    type="submit"
-                    onClick={(e) => handleSubmit(e)}
-                >
-                    Login
-                </Button>
-                {login ? (
-                    <p className="text-success">You Are Logged in Successfully</p>
-                ) : (
-                    <p className="text-danger">You Are Not Logged in</p>
-                )}
-            </Form>
-        </>
+                    {/* submit button */}
+                    <Button
+                        variant="primary"
+                        type="submit"
+                        onClick={(e) => handleSubmit(e)}
+                    >
+                        Login
+                    </Button>
+                    {login ? (
+                        <p className="text-success">You Are Logged in Successfully</p>
+                    ) : (
+                        <p className="text-danger">You Are Not Logged in</p>
+                    )}
+                    <label>Don't have an account? <a href='/signup'>Sign Up</a> now</label>
+                </Form>
+            </div>
+        </div>
     )
 }
